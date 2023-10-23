d676309d1cfc238e2fec1d0e5c2ae6f28833624e
Corentin_k_ <Corentin-k@users.noreply.github.com>
2023-10-23 17:38:16 +0200

    Modif fichier.c

--- fichier.c
+++ fichier.c
@@ -3,3 +3,36 @@
 //
 
 #include "fichier.h"
+#include "stdlib.h"
+
+
+//Créer une cellule : on donne sa valeur et le nombre de niveaux que possède cette
+//cellule, pour obtenir un pointeur vers cette cellule
+t_d_cell *create_d_cell(int value, int nb_levels) {
+
+
+    t_d_cell *cell = NULL;
+
+    cell->value = value;
+    cell->level = nb_levels;
+
+    cell->next = (t_d_cell **)malloc(nb_levels * sizeof(t_d_cell *)); //tableau dynamique de nb_level pointeurs.
+
+    for (int i = 0; i < nb_levels; i++) { //initialisation des pointeurs à NULL
+        cell->next[i] = NULL;
+    }
+    return cell;
+}
+
+
+
+t_d_list *create_d_list(int max_levels) {
+    t_d_list *list = (NULL);
+    list->max_levels = max_levels;
+    list->heads = (t_d_cell **)malloc(max_levels * sizeof(t_d_cell *));
+
+    for (int i = 0; i < max_levels; i++)  { //initialisation des head à NULL
+        list->heads[i] = NULL;
+    }
+    return list;
+}
