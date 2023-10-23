e41030dcd8dec8a835f953731adecfda489211c2
Corentin_k_ <Corentin-k@users.noreply.github.com>
2023-10-23 17:39:56 +0200

    Modif fichier.h

--- fichier.h
+++ fichier.h
@@ -5,4 +5,26 @@
 #ifndef GESTIONNAIRE_AGENDA_FICHIER_H
 #define GESTIONNAIRE_AGENDA_FICHIER_H
 
+
+//Structure cellule stocke une valeur de type en entier et deux pointeurs vers deux autres cellules
+typedef struct s_d_cell
+{
+    int value;
+    int level;
+    struct s_d_cell **next; // stocke un tableau de pointeurs vers des cellules/ double pointeur pour avoir acces directement en faisant cell->next[i]
+
+} t_d_cell;
+
+//Structure liste doublement chainée stocke deux pointeurs vers deux cellules
+typedef struct s_d_list
+{
+    t_d_cell **heads; // Tableau de pointeurs vers les têtes de chaque niveau / double pointeur pour avoir acces directement en faisant list->heads[i]
+    int max_levels; // Nombre maximal de niveaux
+} t_d_list;
+
+t_d_cell *create_d_cell(int value, int num_levels);
+
+t_d_list *create_d_list(int max_levels);
+
+void add_cell_in_list(t_d_list *list, t_d_cell *cell);
 #endif //GESTIONNAIRE_AGENDA_FICHIER_H
