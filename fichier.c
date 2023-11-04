f0fa21f220f22c86b65ac1aca628eb7dfef2dc94
Corentin_k_ <Corentin-k@users.noreply.github.com>
2023-11-04 16:32:23 +0100

    Ajout des fichiers research.c/.h et une fonction pour le calcul du temps

--- fichier.c
+++ fichier.c
@@ -56,32 +56,33 @@ void displayList(t_d_list *list) {
         t_d_cell *precedent_aficher = NULL;
 
         while (temp != NULL) {
-            if (i!=0 ) {
+            if (i != 0) {
                 int nb_espace_requis = 0;
 
                 // La valeur a afficher est la premiere du niveau donc on compte le nombre d'espace entre la premiere cellule du premier niveaux et la cellule que l'on veut affciher
-                if (precedent_aficher== NULL) {
+                if (precedent_aficher == NULL) {
 
 
                     t_d_cell *temp2 = list->heads[0];
                     // on regarde donc dans head[0] quelle est la premiere cellule qui est sur le niveau i+1. i+i Car les niveaux commencent à 1 et les indices des niveaux à 0
-                    while (temp2->level< i+1 && temp2!=NULL) {
+                    while (temp2->level < i + 1 && temp2 != NULL) {
 
-                        temp2 = temp2->next[0];
+                        temp2 = temp2->next[i];
                         nb_espace_requis++;
                     }
-                }
-                else{
+                } else {
                     //cherche le precdent afficher et compte le nombre d'espace entre le prochain a afficher et affiche le nombre d'espace necessaire
 
                     t_d_cell *temp2 = list->heads[i];
 
-                    while (temp2 != precedent_aficher && temp2!=NULL) { // On avance jusqu'a la cellule precedemment afficher
+                    while (temp2 != precedent_aficher &&
+                           temp2 != NULL) { // On avance jusqu'a la cellule precedemment afficher
 
                         temp2 = temp2->next[0];
                     }
 
-                    while (temp2 != temp && temp2!=NULL) { // On avance jusqu'a la cellule a afficher et on compte donc le nombre d'espace entre ces deux valeurs
+                    while (temp2 != temp && temp2 !=
+                                            NULL) { // On avance jusqu'a la cellule a afficher et on compte donc le nombre d'espace entre ces deux valeurs
                         temp2 = temp2->next[0];
                         nb_espace_requis++;
                     }
@@ -90,7 +91,7 @@ void displayList(t_d_list *list) {
 
 
                 for (int g = 0; g < nb_espace_requis; g++) {
-
+                    printf("-");
                     printf("-----------");
                 }
             }
@@ -98,12 +99,16 @@ void displayList(t_d_list *list) {
             displayCell(temp);
 
 
-            precedent_aficher=temp;
-            temp=temp->next[i];
-        }printf("-->NULL\n");
+            precedent_aficher = temp;
+            temp = temp->next[i];
+        }
+        int nb_espace_requis = 0;
+        displayCell(precedent_aficher);
+        
+        printf("-->NULL\n");
+
+
     }
-
-
 }
 
 
@@ -220,47 +225,8 @@ void ajoutrecursiflist(t_d_list* list, int val, int niv,int puiss)
 {// le principe est d'ajouter du niveaux le plus profond et de remonter comme un arbre avec ses voisins
     if (niv==0) return; // condition d'arrêt
     add_cell_in_list(list,create_d_cell(val,niv));
-    printf("ok val : %d",val);
+
     ajoutrecursiflist(list,val+ pow(2,puiss-1),niv-1,puiss-1);
     ajoutrecursiflist(list,val- pow(2,puiss-1),niv-1,puiss-1);
 }
 
-int rechercheclassique(t_d_list* list, int val)
-{
-    t_d_cell* temp = list->heads[0]; // recherche classique d'un élément au niveau 0
-    while (temp != NULL) // on cherche et on renvoit 0 si on a pas trouvé
-    {
-        if (temp->value == val) return val;
-        temp = temp->next[0];
-    }
-    return 0; // 0 == pas trouvé valeur renvoyé == trouvé
-}
-int rechercheintello(t_d_list* list,int val,int niv,t_d_cell* start,t_d_cell* save)
-{
-    if (start->value == val ) return val;
-    if (niv == -1) return 0;
-    if (val > start->value)
-    {
-        //printf("%d",start->next[niv-1]->value);
-        rechercheintello(list,val,niv-1,start->next[niv-1],start);
-    }
-    else
-    {
-        if (start == list->heads[niv])
-        {
-            rechercheintello(list,val,niv-1,list->heads[niv-1],save);
-        }
-        else
-        {
-            t_d_cell* temp = save;
-            while (temp->next[niv-1] != start) temp=temp->next[niv-1];
-            rechercheintello(list,val,niv-1,temp,save);
-            //printf("valeur de temp : %d valeur de start : %d",temp->value,start->value);
-        }
-    }
-}
-
-int rechercheint(t_d_list* list, int val)
-{
-    return rechercheintello(list,val,list->max_levels -1 ,list->heads[list->max_levels - 1],list->heads[list->max_levels -1]);
-}
--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include "fichier.h"
+#include "research.h"
 int main() {
     printf("Hello, test 2!\n");
 
@@ -11,19 +12,22 @@ int main() {
     t_d_cell *cell2 = create_d_cell(20, 2);
     t_d_cell *cell3 = create_d_cell(15, 4);
     t_d_cell *cell4 = create_d_cell(30, 4);
-    t_d_cell *cell5 = create_d_cell(1, 1);
+    t_d_cell *cell5 = create_d_cell(1, 5);
+    t_d_cell *cell6 = create_d_cell(-1101, 5);
     add_cell_in_list(list, cell2);
     add_cell_in_list(list, cell3);
     add_cell_in_list(list, cell4);
     add_cell_in_list(list, cell5);
+    add_cell_in_list(list, cell6);
     // Afficher le contenu de la liste
     displayList(list);
 
-    t_d_list* list2 = createlisttrie(4);
+    t_d_list* list2 = createlisttrie(10);
     printf("clear");
-    displayList(list2);
+    printf("cette valeur est dans la liste tri different :%d", rechercheint(list,20));
+    timesearch();
     //printf("cette valeur est dans la liste :%d", rechercheclassique(list2,20)); printf("clear %d",list2->max_levels);
-printf("cette valeur est dans la liste tri different :%d", rechercheint(list2,7));
+
 
 
 
